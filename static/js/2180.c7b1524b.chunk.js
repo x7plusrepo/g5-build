0443ea621475e8fcdbf9b650271e797b5dc6049f
x7plusrepo <x7plusrepo@gmail.com>
2023-07-01 07:59:49 +0700

    Update camera capture

--- static/js/2180.c7b1524b.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject_g5=self.webpackChunkproject_g5||[]).push([[2180],{2180:function(n,e,t){t.d(e,{Z:function(){return i}});t(7313);var r,c=t(168),u=t(3115).ZP.p(r||(r=(0,c.Z)(["\n  font-size: 13px;\n  font-weight: 500;\n"]))),f=t(6417),i=function(n){var e=n.label;return(0,f.jsx)(u,{children:e})}}}]);
